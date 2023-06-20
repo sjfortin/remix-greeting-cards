f2bbc1f4e63ecfc550429733fe76b58afbbc1a73
sjfortin <sam.j.fortin@gmail.com>
2023-06-20 11:56:40 -0500

    start with tailwind styles

--- app/routes/cards.new.tsx
+++ app/routes/cards.new.tsx
@@ -8,7 +8,7 @@ export const action = async ({ request }: ActionArgs) => {
   const cardRecipient = form.get("cardRecipient");
   const insideContent = form.get("insideContent");
   const frontContent = form.get("frontContent");
-  
+
   if (
     typeof cardRecipient !== "string" ||
     typeof insideContent !== "string" ||
@@ -26,25 +26,85 @@ export const action = async ({ request }: ActionArgs) => {
 export default function NewCardRoute() {
   return (
     <div>
-      <p>Add your own card:</p>
+      <h2 className="text-base font-semibold leading-7 text-gray-900">
+        Add your own card content:
+      </h2>
+      <p className="mt-1 text-sm leading-6 text-gray-600">
+        This information will be displayed publicly so be careful what you
+        share.
+      </p>
       <form method="post">
-        <div>
-          <label>
-            Card Recipient: <input type="text" name="cardRecipient" />
-          </label>
+        <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="cardRecipient"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Card Recipient
+            </label>
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="cardRecipient"
+                  id="cardRecipient"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full">
+            <label
+              htmlFor="frontContent"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Front Content
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="frontContent"
+                name="frontContent"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600">
+              Add content for the front of the card.
+            </p>
+          </div>
+          <div className="col-span-full">
+            <label
+              htmlFor="insideContent"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Inside Content
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="insideContent"
+                name="insideContent"
+                rows={3}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
+            <p className="mt-3 text-sm leading-6 text-gray-600">
+              Add content for the inside of the card.
+            </p>
+          </div>
         </div>
-        <div>
-          <label>
-            Front Content: <textarea name="frontContent" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Inside Content: <textarea name="insideContent" />
-          </label>
-        </div>
-        <div>
-          <button type="submit" className="button">
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
             Add
           </button>
         </div>
--- app/routes/cards.tsx
+++ app/routes/cards.tsx
@@ -1,12 +1,40 @@
 import { json } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData, useMatches } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { classNames } from "~/utils/helpers";
+
+type CardListItemProps = {
+  id: string;
+  cardRecipient: string;
+};
 
 export const loader = async () => {
   return json({
     cardListItems: await db.card.findMany(),
   });
+};
+
+const CardListItem: React.FC<CardListItemProps> = ({ id, cardRecipient }) => {
+  const matches = useMatches();
+  const matchID = matches[matches.length - 1];
+  const isCurrent = matchID.params.cardId === id;
+
+  return (
+    <li>
+      <Link
+        className={classNames(
+          isCurrent
+            ? "bg-gray-50 text-indigo-600"
+            : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+          "group flex gap-x-3 rounded-md p-2 pl-3 text-sm leading-6 font-semibold"
+        )}
+        to={id}
+      >
+        {cardRecipient}
+      </Link>
+    </li>
+  );
 };
 
 export default function CardsRoute() {
@@ -15,20 +43,24 @@ export default function CardsRoute() {
   return (
     <div>
       <main>
-        <div>
-          <div>
-            <ul>
+        <div className="flex">
+          <div className="w-1/4 p-4 border rounded">
+            <h2 className="text-xl sm:text-2xl font-bold tracking-tight text-gray-900">
+              List of Card Recipients:
+            </h2>
+            <ul className="space-y-1 my-3">
               {data.cardListItems.map(({ id, cardRecipient }) => (
-                <li key={id}>
-                  <Link to={id}>{cardRecipient}</Link>
-                </li>
+                <CardListItem key={id} id={id} cardRecipient={cardRecipient} />
               ))}
             </ul>
-            <Link to="new" className="button">
+            <Link
+              to="new"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
               Add your own
             </Link>
           </div>
-          <div>
+          <div className="w-3/4 p-4">
             <Outlet />
           </div>
         </div>
